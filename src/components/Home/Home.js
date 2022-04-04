6d09f572a15a38755d2946ea0272064ee6fd2dd2
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-04-03 19:30:44 -0600

    Push what I have so far

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 // import AddRouteContainer from '../../redux/containers/AddRouteContainer';
 
@@ -7,16 +7,17 @@ import { connect } from 'react-redux';
 import { addRoute } from '../../redux/actions/index';
 
 
-function Home({routes, handleSubmit})  {
+function Home(props)  {
+  const [testId, setTestId] = useState(0);
    
   useEffect(() => {
     console.log('home mounted');
     console.log(data)
-    console.log('props', handleSubmit)
+    console.log('props', props)
   });
 
 
-  const data = useSelector((state) => state.routesReducer);
+  const data = useSelector((state) => state.routes);
 
   let routeId;
 
@@ -26,8 +27,8 @@ function Home({routes, handleSubmit})  {
         Home components???????????????
         <form id="routeId">
           <label>Add Route</label>
-          <input type="text" id="routeId" value={routeId} placeholder="route-id"></input>
-          <button type="submit" form="routeId" onClick={() => handleSubmit()}></button>
+          <input type="text" id="routeId" placeholder="route-id"></input>
+          <button type="submit" form="routeId"></button>
         </form>
       </div>
   )
@@ -43,11 +44,13 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     // this handleSubmit is what is being expected to be return from the component it belongs to
     handleSubmit: (routeId) => {
+      console.log('routeId', routeId)
       dispatch(addRoute(routeId))
-  }
+  }, 
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
+// export default Home
 
 
 
