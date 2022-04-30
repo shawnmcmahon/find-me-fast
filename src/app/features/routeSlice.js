3c4314e5305e6d199e97694b351f33acaa49d32a
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-04-30 13:43:54 -0600

    Add reducers to the route slice

--- src/app/features/routeSlice.js
+++ src/app/features/routeSlice.js
@@ -9,8 +9,24 @@ const routeSlice = createSlice({
       {routeId: 2}, 
       {routeId: 3}
     ]
-  }, 
-  
-
-
+  },
+  reducers: {
+    setLoading: (state) => {
+      if (state.loading === false) {
+        state.loading = true;
+      };
+    }, 
+    setRoutes: (state, action) => {
+      if (state.loading === true) {
+        state.routes = action.payload
+        state.loading = false;
+      }; 
+    }
+  }
 })
+
+const { actions, reducer } = routeSlice; 
+
+export const { setLoading, setRoutes} = actions; 
+
+export default reducer; 
